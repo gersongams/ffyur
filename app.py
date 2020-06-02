cdc2771d451d855f3fc7bb91c94eb1fd23086609
Gerson Garrido <ggarridom23@gmail.com>
2020-06-02 04:02:58 -0500

    finishing project

---
+++ app.py
@@ -0,0 +1,211 @@
+# ----------------------------------------------------------------------------#
+# Imports
+# ----------------------------------------------------------------------------#
+
+import logging
+from logging import Formatter, FileHandler
+
+import babel
+import dateutil.parser
+from flask import render_template, request, redirect, url_for
+
+from controllers import VenueController, ArtistController, ShowsController
+from forms import *
+from models import *
+
+# ----------------------------------------------------------------------------#
+# Filters.
+# ----------------------------------------------------------------------------#
+
+venue_controller = VenueController(db, Venue, Artist, Show)
+artist_controller = ArtistController(db, Venue, Artist, Show)
+show_controller = ShowsController(db, Venue, Artist, Show)
+
+
+def format_datetime(value, format='medium'):
+    date = dateutil.parser.parse(value)
+    if format == 'full':
+        format = "EEEE MMMM, d, y 'at' h:mma"
+    elif format == 'medium':
+        format = "EE MM, dd, y h:mma"
+    return babel.dates.format_datetime(date, format)
+
+
+app.jinja_env.filters['datetime'] = format_datetime
+
+
+# ----------------------------------------------------------------------------#
+# Controllers.
+# ----------------------------------------------------------------------------#
+
+
+@app.route('/')
+def index():
+    return render_template('pages/home.html')
+
+
+#  Venues
+#  ----------------------------------------------------------------
+
+@app.route('/venues')
+def venues():
+    return render_template('pages/venues.html', areas=venue_controller.get_all_venues())
+
+
+@app.route('/venues/search', methods=['POST'])
+def search_venues():
+    response = venue_controller.search_venue()
+    return render_template('pages/search_venues.html', results=response,
+                           search_term=request.form.get('search_term', ''))
+
+
+@app.route('/venues/<int:venue_id>')
+def show_venue(venue_id):
+    return render_template('pages/show_venue.html', venue=venue_controller.get_venue(venue_id))
+
+
+#  Create Venue
+#  ----------------------------------------------------------------
+
+
+@app.route('/venues/create', methods=['GET'])
+def create_venue_form():
+    form = VenueForm()
+    return render_template('forms/new_venue.html', form=form)
+
+
+@app.route('/venues/create', methods=['POST'])
+def create_venue_submission():
+    venue_controller.create_venue()
+    return render_template('pages/home.html')
+
+
+@app.route('/venues/<venue_id>', methods=['DELETE'])
+def delete_venue(venue_id):
+    venue_controller.delete_venue(venue_id)
+    return render_template('pages/home.html')
+
+
+#  Artists
+#  ----------------------------------------------------------------
+
+
+@app.route('/artists')
+def artists():
+    return render_template('pages/artists.html', artists=artist_controller.get_all_artists())
+
+
+@app.route('/artists/search', methods=['POST'])
+def search_artists():
+    response = artist_controller.search_artist()
+    return render_template('pages/search_artists.html', results=response,
+                           search_term=request.form.get('search_term', ''))
+
+
+@app.route('/artists/<int:artist_id>')
+def show_artist(artist_id):
+    data = artist_controller.get_artist(artist_id)
+    return render_template('pages/show_artist.html', artist=data)
+
+
+#  Update
+#  ----------------------------------------------------------------
+
+
+@app.route('/artists/<int:artist_id>/edit', methods=['GET'])
+def edit_artist(artist_id):
+    form, artist = artist_controller.send_edit_form(artist_id)
+    return render_template('forms/edit_artist.html', form=form, artist=artist)
+
+
+@app.route('/artists/<int:artist_id>/edit', methods=['POST'])
+def edit_artist_submission(artist_id):
+    artist_controller.edit_artist(artist_id)
+    return redirect(url_for('show_artist', artist_id=artist_id))
+
+
+@app.route('/venues/<int:venue_id>/edit', methods=['GET'])
+def edit_venue(venue_id):
+    form, venue = venue_controller.send_edit_venue_form(venue_id)
+    return render_template('forms/edit_venue.html', form=form, venue=venue)
+
+
+@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
+def edit_venue_submission(venue_id):
+    venue_controller.edit_venue(venue_id)
+    return redirect(url_for('show_venue', venue_id=venue_id))
+
+
+#  Create Artist
+#  ----------------------------------------------------------------
+
+
+@app.route('/artists/create', methods=['GET'])
+def create_artist_form():
+    form = ArtistForm()
+    return render_template('forms/new_artist.html', form=form)
+
+
+@app.route('/artists/create', methods=['POST'])
+def create_artist_submission():
+    artist_controller.create_artist()
+    return render_template('pages/home.html')
+
+
+#  Shows
+#  ----------------------------------------------------------------
+
+@app.route('/shows')
+def shows():
+    data = show_controller.get_all_shows()
+    return render_template('pages/shows.html', shows=data)
+
+
+@app.route('/shows/create')
+def create_shows():
+    # renders form. do not touch.
+    form = ShowForm()
+    return render_template('forms/new_show.html', form=form)
+
+
+@app.route('/shows/create', methods=['POST'])
+def create_show_submission():
+    show_controller.create_show()
+    return render_template('pages/home.html')
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('errors/500.html'), 500
+
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+# ----------------------------------------------------------------------------#
+# Launch.
+# ----------------------------------------------------------------------------#
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
